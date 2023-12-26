e373b8da1ab03ceee46a2b27f737ec54f1df0c68
Dagmawi <qdagmawi@gmail.com>
2023-12-26 22:09:00 +0300

    Records added and a new column username for the table author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_26_185654) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_26_190205) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.text "description"
